dc2836a1141048fb11c3602a9a9f5c550355db0b
nehad222 <nihadshrith@gmail.com>
2025-02-20 17:27:43 +0000

    sads

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import './App.css';
-import TelegramAuth from './components/TeleAuth';
-function App() {
+import { useEffect, useState } from "react";
+import Error_auth from "./Error_auth";
+
+const TelegramAuth = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+
+    if (window.Telegram && window.Telegram.WebApp) {
+      const tg = window.Telegram.WebApp;
+      
+     
+      if (tg.initDataUnsafe && tg.initDataUnsafe.user) {
+        setUser(tg.initDataUnsafe.user);
+      } else {
+      }
+    }
+  }, []);
 
   return (
-    <>
-     <div>
-      <TelegramAuth/>
-     </div>
-    </>
-  )
-}
+    <div>
+      {user ? (
+        <div>
+          <h1>OK</h1>
+        </div>
+      ) : (
+        <Error_auth />
+      )}
+    </div>
+  );
+};
 
-export default App
+export default TelegramAuth;
